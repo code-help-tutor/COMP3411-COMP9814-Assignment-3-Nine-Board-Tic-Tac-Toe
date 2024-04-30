8f212476184e17663c22a36362f8aa9120d616fe
Code Help Tutor <1823890830@qq.com>
2024-04-30 05:43:54 +0000

    WeChat cstutorcs COMP3411 COMP9814 Assignment 3 – Nine Board Tic Tac Toe

---
+++ playt.sh
@@ -0,0 +1,19 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#!/bin/bash
+
+# Play agent against specified program
+# Example:
+# ./playt.sh ./agent lookt 12345
+# ./playt.sh "python3 agent.py" ./lookt 12345
+# ./playt.sh "java Agent" ./lookt 12345
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <player1> <player2> <port>" >&2
+  exit 1
+fi
+
+./servt -p $3 & sleep 0.5
+$1 -p $3 & sleep 0.5
+$2 -p $3
